54959029c6d4f97d696c3ba83b22074cf03f4690
Lilla Kertesz <lil.kertesz@gmail.com>
2020-10-17 18:51:08 +0200

    starship details styling

--- src/app/models/starship.ts
+++ src/app/models/starship.ts
@@ -1,11 +1,11 @@
 export interface Starship {
-    name: string;
-    model: string;
-    manufacturer: string;
-    length: string;
-    max_atmosphering_speed: string;
-    crew: string;
-    passengers: string;
-    starship_class: string;
-
+  name: string;
+  model: string;
+  manufacturer: string;
+  length: string;
+  max_atmosphering_speed: string;
+  crew: string;
+  passengers: string;
+  starship_class: string;
+  consumables: string;
   }
--- src/app/models/starshipdetail.ts
+++
@@ -1,8 +0,0 @@
-export interface Starship {
-  model: string;
-  manufacturer: string;
-  length: string;
-  max_atmosphering_speed: string;
-  crew: string;
-  passengers: string;
-}
