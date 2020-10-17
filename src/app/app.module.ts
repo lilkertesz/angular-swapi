434fa1043d56ae3d617ff192de11537ef09734ac
Lilla Kertesz <lil.kertesz@gmail.com>
2020-10-17 12:35:22 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { StarshipDetailComponent } from './starship-detail/starship-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StarshipsComponent,
+    StarshipDetailComponent
   ],
   imports: [
     BrowserModule
